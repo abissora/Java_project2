170f312b753af944ea0276cd822a823e8c1762cf
Alka <alka.bissora@gmail.com>
2021-02-27 21:37:50 +0000

    first commit

---
+++ src/addition.java
@@ -0,0 +1,16 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		
+		int i, j=0;
+		for (i=0; i<100; i++)
+		{
+			j=j+i;
+		}
+		System.out.println("Sum of first 100 numbers: " + j);
+		// TODO Auto-generated method stub
+
+	}
+
+}
